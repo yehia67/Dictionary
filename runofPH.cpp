cf0b4ecee23a584a8e1510a7fdb55be09c1d25de
yehia67 <yehiatarek67@gmail.com>
2017-04-23 22:14:41 +0200

    german and add
    we add Gernan dectionary option and incomplete add fuction

--- runofPH.cpp
+++ runofPH.cpp
@@ -3,10 +3,18 @@
 #include<string>
 #include<fstream>
 using namespace std;
+void add(){
+	ifstream addE,addF;
+	addE.open("english.txt");
+	addF.open("frensh.txt");
+	
+}
 int main(){
-	ifstream englishword,frenshword;
+	ifstream englishword,frenshword,englishGword,Germanword;
 	englishword.open("english.txt");
-	frenshword.open("frensh.txt");	
+	frenshword.open("frensh.txt");
+	Germanword.open("german.txt");	
+	englishGword.open("englishG.txt");
 	if(englishword.fail()){
 		cerr <<"Error Opening english text file"<<endl;
 		exit(1);
@@ -15,34 +23,51 @@ int main(){
 		cerr <<"Error Opening Frensh text file"<<endl;
 		exit(1);
 	}
+	if(Germanword.fail()){
+		cerr <<"Error Opening german text file"<<endl;
+		exit(1);
+	}
+	if(englishGword.fail()){
+		cerr <<"Error Opening englishG text file"<<endl;
+		exit(1);
+	}
 	int x;
 //	printf("please enter 1 if u want to translate from English to Arabic or enter 2 if u want to translate from Arabic to English \n ");
     cout<<"enter 1 English to frensh"<<endl;
-	cout<<"enter 2 Frensh to english"<<endl;	
+	cout<<"enter 2 Frensh to english"<<endl;
+	cout<<"enter 3 English to German"<<endl;	
 	cin>>x;
-	while(x != 1 && x!= 2)
+	while(x != 1 && x!= 2 && x != 3 )  
 {
-	printf("Error enter 1 or 2 Only!! \n ");
+	printf("Error enter 1 2 or 3 Only!! \n ");
 	scanf("%d",&x);
 		}		
 		printf("enter the word\n ");
 		string s;
 		cin>>s;
 		map <string,string> m;
-		string englishItem,frenchItem;
+		string englishItem,forgeinItem;
 		
 		while(!(englishword.eof()) && !(frenshword.eof()))
 {
 	englishword >> englishItem;
-	getline(frenshword,frenchItem);
+	getline(frenshword,forgeinItem);
 	if(x == 1)
 	{
-		m[englishItem] = frenchItem;
+		m[englishItem] = forgeinItem;
 	}
-else
+else if(x == 2)
 {
-	m[frenchItem] = englishItem ;
+	m[forgeinItem] = englishItem ;
 }
+}
+while(!(englishGword.eof()) && !(Germanword.eof())){
+	englishGword >> englishItem;
+  getline(Germanword,forgeinItem);
+	if(x == 3)
+	{
+		m[englishItem] = forgeinItem;
+	}
 }
  cout<<m[s]<<endl;
  englishword.close();
